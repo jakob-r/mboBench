512cf638a4938b8aa92853a421386cfbfff58906
Jakob Richter <code@jakob-r.de>
2017-12-06 16:32:14 +0100

    add BenchmarkExpensive

---
+++ R/BenchmarkExpensive.R
@@ -0,0 +1,21 @@
+#' @export
+BenchmarkExpensive = R6Class(
+  classname = "BenchmarkExpensive",
+  inherit = Benchmark,
+
+  public = list(
+    # member variables
+    resources.walltime = NULL,
+    resources.memory = NULL,
+    resources.cpus = NULL,
+
+    # constructor
+    initialize = function(id = NULL, smoof.fun, termination.criterions, expensive = FALSE, threasholds, initial.designs = NULL, initial.design.n = NULL, tags = character(0L), values = NULL, resources.cpus = 1, resources.memory = 256, resources.walltime = 1) {
+
+      self$resources.cpus = assertInt(resources.cpus, lower = 1)
+      self$resources.memory = assertInt(resources.memory, lower = 256)
+      self$resources.walltime = assertInt(resources.walltime, lower = 1)
+
+    }
+  )
+)
