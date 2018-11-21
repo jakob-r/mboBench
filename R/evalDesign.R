437379efb7a56aed26a625ce78de1442976556c9
Jakob Richter <code@jakob-r.de>
2018-11-21 13:48:15 +0100

    fix na handling a bit

--- R/evalDesign.R
+++ R/evalDesign.R
@@ -10,8 +10,9 @@ evalDesign = function(design, smoof.fun) {
   assertClass(smoof.fun, "smoof_function")
 
   par.set = getParamSet(smoof.fun)
+  sub.design = design[, getParamIds(par.set, repeated = TRUE, with.nr = TRUE), drop = FALSE]
 
-  xs = dfRowsToList(design[, getParamIds(par.set, repeated = TRUE, with.nr = TRUE), drop = FALSE], par.set)
+  xs = dfRowsToList(sub.design, par.set, enforce.col.types = TRUE) #FIXME changes NA_numeric to NA_logical! (1)
 
   if (hasTrafo(par.set)) {
     xs.trafo = lapply(xs, trafoValue, par = par.set)
@@ -20,6 +21,7 @@ evalDesign = function(design, smoof.fun) {
   }
   ys = foreach(x = xs.trafo) %do% {
     st = proc.time()
+    x = x[!sapply(x, is.na)] # Should avoid problems with (1)
     y = smoof.fun(x)
     st = proc.time() - st
     list(y = y, time = st[3])
